2624fed012177fa01aba44b0de3b8c7a3bb877a9
edgaguil <edgaguil@COBOGPGP1556>
2018-08-01 16:22:18 -0500

    Se agregan los archivos iniciales

---
+++ src/Services/AAB.MSA.Shopping.ProductCatalogAPI/Domain/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAB.MSA.Shopping.ProductCatalogAPI.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Services/AAB.MSA.Shopping.ProductCatalogAPI/Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAB.MSA.Shopping.ProductCatalogAPI.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureFileName { get; set; }
+        public string PictureUrl { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public Category Category { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
